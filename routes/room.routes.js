116f352974ffd834f760d3ca8bb20a0dc636028e
evgenyvodyannikov <vodyannikov.eugene@gmail.com>
2023-09-18 23:50:22 +0500

    update AbailableRooms query

--- routes/room.routes.js
+++ routes/room.routes.js
@@ -7,6 +7,7 @@ roomRouter.get('/rooms', async (req, res) => {
     try {
         const query = 'SELECT * FROM room';
         const { rows } = await pool.query(query);
+
         res.json(rows);
     } catch (error) {
         console.error('Error fetching rooms', error);
@@ -16,8 +17,13 @@ roomRouter.get('/rooms', async (req, res) => {
 
 roomRouter.get('/AvailableRooms', async (req, res) => {
     try {
-        const query = 'SELECT * FROM available_room Where is_available = TRUE';
-        const { rows } = await pool.query(query);
+        const roomId = req.query.roomId;
+        const startDate = req.query.startDate;
+        const endDate = req.query.endDate;
+
+        const query = `SELECT room_id FROM booking Where room_id = $1 And NOT (start_date BETWEEN $2 And $3) And NOT (end_date BETWEEN $2 And $3)`;
+        const { rows } = await pool.query(query, [roomId, startDate, endDate]);
+
         res.json(rows);
     } catch (error) {
         console.error('Error fetching rooms', error);
